b72a9a9e02dae599246a6ebd8ad8c27dadcf2d5d
ZERZORER <a01371255@itesm.mx>
2016-09-16 08:24:49 -0500

    Update orden_controller.rb

--- app/controllers/orden_controller.rb
+++ app/controllers/orden_controller.rb
@@ -1,2 +1,26 @@
 class OrdenController < ApplicationController
+    
+#----Interfaz----
+  def new
+  end
+  
+  def edit
+  end
+#------------------  
+  
+  def create
+  end
+
+  def destroy
+  end
+  
+  def update
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
 end
