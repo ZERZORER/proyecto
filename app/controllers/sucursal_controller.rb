69d072d1965f4c48b63a79eb0b8f8171abfcd293
ZERZORER <a01371255@itesm.mx>
2016-09-16 09:52:52 -0500

    Update sucursal_controller.rb

--- app/controllers/sucursal_controller.rb
+++ app/controllers/sucursal_controller.rb
@@ -5,22 +5,58 @@ class SucursalController < ApplicationController
   end
   
   def edit
+      received_id = params[:id]
+      @sucursal = Sucursal.where(id: received_id).first
   end
 #------------------  
   
   def create
+      t = Sucursal.new({
+          nombre: params[:nombre],
+          direccion: params[:direccion],
+          telefono: params[:telefono],
+          correo: params[:correo]
+      })
+      
+      t.save
   end
 
   def destroy
+    received_id = params[:id].to_i
+    t = Sucursal.where(id: received_id).first
+    if t.nil?
+        return redirect_to action: :index
+    else
+        t.destroy
+        return redirect_to action: :index
+    end
+
   end
   
   def update
+      received_id = params[:id]
+        @sucursal = Sucursal.where(id: received_id).first
+
+        @sucursal.update_attributes({
+            nombre: params[:nombre],
+            direccion: params[:direccion],
+            telefono: params[:telefono],
+            correo: params[:correo]
+        })
+
+        redirect_to action: :show
+
   end
   
   def index
   end
   
   def show
+      received_id = params[:id]
+      @sucursal = Sucursal.where(id: received_id).first
   end
+  
+end
+
   
 end
