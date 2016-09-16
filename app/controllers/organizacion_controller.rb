b1aa7c4c886d16a11b3d5306c4f085859be88ce8
ZERZORER <a01371255@itesm.mx>
2016-09-16 09:33:40 -0500

    Update organizacion_controller.rb

--- app/controllers/organizacion_controller.rb
+++ app/controllers/organizacion_controller.rb
@@ -5,22 +5,55 @@ class OrganizacionController < ApplicationController
   end
   
   def edit
+      received_id = params[:id]
+      @orga = Organizacion.where(id: received_id).first
   end
 #------------------  
   
   def create
+      t = Organizacion.new({
+          nombre: params[:nombre],
+          direccion: params[:direccion],
+          telefono: params[:telefono],
+          correo: params[:correo]
+      })
+      
+      t.save
   end
 
   def destroy
+    received_id = params[:id].to_i
+    t = Organizacion.where(id: received_id).first
+    if t.nil?
+        return redirect_to action: :index
+    else
+        t.destroy
+        return redirect_to action: :index
+    end
+
   end
   
   def update
+      received_id = params[:id]
+        @orga = Task.where(id: received_id).first
+
+        @orga.update_attributes({
+            nombre: params[:nombre],
+            direccion: params[:direccion],
+            telefono: params[:telefono],
+            correo: params[:correo]
+        })
+
+        redirect_to action: :show
+
   end
   
   def index
   end
   
   def show
+      received_id = params[:id]
+      @orga = Organizacion.where(id: received_id).first
   end
   
 end
